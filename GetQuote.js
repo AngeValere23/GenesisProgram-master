4519fbd0277173c19fffcf65319319cfd607f9c3
AngeValere23 <63755538+AngeValere23@users.noreply.github.com>
2020-06-11 23:35:08 -0400

    modification of GetQuote.js

--- GetQuote.js
+++ GetQuote.js
@@ -1,40 +1,189 @@
-function fonctionChoice_dropMenu() {
-	var x = document.getElementById("dropMenu");
-	var i = x.selectedIndex;						  
-	return i;
+
+	function AddElementResidQs1() {
+	document.getElementById("ResidQs1").style.display = "block";}
+	function AddElementResidQs2() {
+	document.getElementById("ResidQs2").style.display = "block";}
+	function AddElementResidQs3() {
+	document.getElementById("ResidQs3").style.display = "block";}
+	function AddElementCommQs1() {
+	document.getElementById("CommQs1").style.display = "block";}
+	function AddElementCommQs2() {
+	document.getElementById("CommQs2").style.display = "block";}
+	function AddElementCommQs3() {
+	document.getElementById("CommQs3").style.display = "block";}
+	function AddElementCorpHybQs1() {
+	document.getElementById("CorpHybQs1").style.display = "block";}
+	function AddElementCorpHybQs2() {
+	document.getElementById("CorpHybQs2").style.display = "block";}
+	function AddElementCorpHybQs3() {
+	document.getElementById("CorpHybQs3").style.display = "block";}						
+	function AddElementResidElevatorQty() {
+	document.getElementById("ResidElevatorQty").style.display = "block";}
+	function AddElementCommElevatorQty() {
+	document.getElementById("CommElevatorQty").style.display = "block";}
+	function AddElementCorpHybElevatorQty() {
+	document.getElementById("CorpHybElevatorQty").style.display = "block";}
 	
-}
-							 
-function Call()  {
-type_Selected();	
+	function RemoveElementResidQs1() {
+	document.getElementById("ResidQs1").style.display = "none";}						
+	function RemoveElementResidQs2() {
+	document.getElementById("ResidQs2").style.display = "none";}						
+	function RemoveElementResidQs3() {
+	document.getElementById("ResidQs3").style.display = "none";}						
+	function RemoveElementCommQs1() {
+	document.getElementById("CommQs1").style.display = "none";}						
+	function RemoveElementCommQs2() {
+	document.getElementById("CommQs2").style.display = "none";}						
+	function RemoveElementCommQs3() {
+	document.getElementById("CommQs3").style.display = "none";}						
+	function RemoveElementCorpHybQs1() {
+	document.getElementById("CorpHybQs1").style.display = "none";}						
+	function RemoveElementCorpHybQs2() {
+	document.getElementById("CorpHybQs2").style.display = "none";}						
+	function RemoveElementCorpHybQs3() {
+	document.getElementById("CorpHybQs3").style.display = "none";}						
+	function RemoveElementResidElevatorQty() {
+	document.getElementById("ResidElevatorQty").style.display = "none";}
+	function RemoveElementCommElevatorQty() {
+	document.getElementById("CommElevatorQty").style.display = "none";}
+	function RemoveElementCorpHybElevatorQty() {
+	document.getElementById("CorpHybElevatorQty").style.display = "none";}
+	
+	//DEFAULT 
+	function mySelection()	 {	
+
+		document.getElementById("qty_Pop").value = "";															  
+		document.getElementById("qty_Appart").value = ""; 
+		document.getElementById("qty_Base").value = ""; 
+		document.getElementById("qty_Business").value = "";
+		document.getElementById("qty_Eleva").value = "";								
+		document.getElementById("qty_Level").value = "";
+		document.getElementById("qty_loan").value = ""; 								
+		document.getElementById("qty_Parking").value = "";								
+		document.getElementById("qty_Hours").value = ""; 
+		document.getElementById("returned_qty_Eleva").value = "";
+		document.getElementById("hybrid").value = "";		
+		document.getElementById("returned_qty_ElevaB").value = "";
+		document.getElementById("PriceUnit").value = "";
+		document.getElementById("InstallationCost").value = "";
+		document.getElementById("Total").value = "";	
+								
+		if (choice_building_type()  === 0)  { 
+			RemoveElementResidQs1();
+			RemoveElementResidQs2();
+			RemoveElementResidQs3();
+			RemoveElementCommQs1();
+			RemoveElementCommQs2();
+			RemoveElementCommQs3();
+			RemoveElementCorpHybQs1();
+			RemoveElementCorpHybQs2();
+			RemoveElementCorpHybQs3(); 
+			RemoveElementResidElevatorQty();
+			RemoveElementCommElevatorQty();
+			RemoveElementCorpHybElevatorQty();
+			}  
+	//RESIDENTIAL		
+		if	(choice_building_type() === 1)	{	
+			AddElementResidQs1();
+			AddElementResidQs2();
+			AddElementResidQs3();
+			AddElementResidElevatorQty()
+			RemoveElementCommQs1();
+			RemoveElementCommQs2();
+			RemoveElementCommQs3();
+			RemoveElementCorpHybQs1();
+			RemoveElementCorpHybQs2();
+			RemoveElementCorpHybQs3(); 
+			RemoveElementCommElevatorQty();
+			RemoveElementCorpHybElevatorQty();
+			  
+			} 
+	//COMMERCIAL											
+		if	(choice_building_type() === 2)	{								
+			AddElementResidQs2();
+			AddElementResidQs3();
+			AddElementCommQs1();
+			AddElementCommQs2();
+			AddElementCommQs3();
+			AddElementCommElevatorQty();
+			RemoveElementResidQs1();
+			RemoveElementCorpHybQs1();
+			RemoveElementCorpHybQs2();
+			RemoveElementCorpHybQs3(); 
+			RemoveElementResidElevatorQty();
+			RemoveElementCorpHybElevatorQty();
+			}  
+	//CORPORATE									
+		if	(choice_building_type() === 3)	{												
+			AddElementResidQs2();
+			AddElementResidQs3();
+			AddElementCommQs2();
+			AddElementCorpHybQs1()
+			AddElementCorpHybQs2()
+			AddElementCorpHybElevatorQty()
+			RemoveElementResidQs1();
+			RemoveElementCommQs1();
+			RemoveElementCommQs3();
+			RemoveElementCorpHybQs3();
+			RemoveElementResidElevatorQty();
+			RemoveElementCorpHybElevatorQty();
+
+		
+			} 
+	//HYBRID
+		if	(choice_building_type() === 4)	{				
+			AddElementResidQs2();
+			AddElementResidQs3();
+			AddElementCommQs1();
+			AddElementCommQs2();
+			AddElementCorpHybQs2()
+			AddElementCorpHybQs3()
+			AddElementCorpHybElevatorQty()
+			RemoveElementResidQs1();
+			RemoveElementCommQs3();
+			RemoveElementCorpHybQs1();
+			RemoveElementResidElevatorQty();
+			RemoveElementCommElevatorQty();
+			}
+
+			// Update All the field once choice_building_type is changed
+												
+	}
+
+	//returns the index of the selected option in the building_type drop-down menu
+	function choice_building_type() {
+		var x = document.getElementById("building_type");
+		var i = x.selectedIndex;						  
+		return i;
+		
+	}
+
+	// call the type will be selected in the drop down menu 
+	function Call()  {
+	type_Selected();	
 
 
-}
+	}
 
 function type_Selected()  {							
-   var service =  fonctionChoice_dropMenu(); 					   
+   var service =  choice_building_type(); 					   
    
    if ( service == 1 )   {
-	returnQuantity_residential(); 
+	residentialCalcul(); 
    }
    
    if ( service == 2 )   {
-	returnQuantity_commercial();						
+	commercialCalcul();						
    }
    
-   if ( service == 3 )   {
-	returnQuantity_corporate_hybrid(); 
-   }
-   
-   if ( service == 4 )   {
-	  returnQuantity_corporate_hybrid(); 
-   }					   
+   if (service == 3 || service == 4 )  {
+	 corporate_hybridCalcul();
+   }		
 }
 
-	//var install_Rate;							   
-	//var Total;
+	
 
-	function returnQuantity_residential() {
+	function residentialCalcul() {
 		//Appart qty 
 			   var qty_Appart = document.getElementById("qty_Appart").value || 0 ;
 		//Level qty 
@@ -61,7 +210,7 @@ function type_Selected()  {
 			return RqtyElevators;
 			}
 
-	function returnQuantity_commercial() {
+	function commercialCalcul() {
 		//Level qty 
 			var CLevel = document.getElementById("qty_Level").value || 0 ;
 		//Basement qty 
@@ -89,9 +238,10 @@ function type_Selected()  {
 			}										
 					
 
-	function returnQuantity_corporate_hybrid() {
-	   service = fonctionChoice_dropMenu();
+	function corporate_hybridCalcul() {
+	   service = choice_building_type();
 
+		//NUMBER() convert the value to number	
 		var HCLevel = Number(document.getElementById("qty_Level").value || 0  );
 		var HCBasement = Number(document.getElementById("qty_Base").value || 0 );
 		var HCOccupant = document.getElementById("qty_Pop").value || 0 ;
@@ -123,188 +273,49 @@ function type_Selected()  {
 
 	
 
-function Price()  
-{
-var service = fonctionChoice_dropMenu();						 
-		 
-if (service == 1)  {
-	 var qty_Elevators_quote = returnQuantity_residential();
-}
- if (service == 2)  {
-	 var qty_Elevators_quote = returnQuantity_commercial();
-}								
- if (service == 3 || service == 4 )  {
-	 var qty_Elevators_quote = returnQuantity_corporate_hybrid();
-}																
-											
-var install_Rate;							   
-var Total;
+	function Price()  
+	{
+	var service = choice_building_type();						 
+			
+	if (service == 1)  {
+		var qty_Elevators_quote = residentialCalcul();
+	}
+	if (service == 2)  {
+		var qty_Elevators_quote = commercialCalcul();
+	}								
+	if (service == 3 || service == 4 )  {
+		var qty_Elevators_quote = corporate_hybridCalcul();
+	}																
+												
+	var install_Rate;							   
+	var Total;
 	 
-if  (document.getElementById("PriceUnitStandard").checked)	{						 
-document.getElementById("PriceUnit").value="7565.00";
-install_Rate = (7565.00*(10/100)*qty_Elevators_quote).toFixed(2);
-Total = (7565.00*1.10*qty_Elevators_quote).toFixed(2);//ok
-document.getElementById("InstallationCost").value = install_Rate ;
-document.getElementById("Total").value = Total;
-} 	
+	// Calcul of Standard choice
+	if  (document.getElementById("PriceUnitStandard").checked)	{						 
+		document.getElementById("PriceUnit").value="7565.00";
+		install_Rate = (7565.00*(10/100)*qty_Elevators_quote).toFixed(2);
+		Total = (7565.00*1.10*qty_Elevators_quote).toFixed(2);
+		document.getElementById("InstallationCost").value = install_Rate ;
+		document.getElementById("Total").value = Total;
+	} 	
 
-if  (document.getElementById("PriceUnitPremium").checked)	{						 
-document.getElementById("PriceUnit").value="12345.00";
-install_Rate = (12345.00*(13/100)*qty_Elevators_quote).toFixed(2);
-Total = (12345.00*1.13*qty_Elevators_quote).toFixed(2);//ok
-document.getElementById("InstallationCost").value = install_Rate ;
-document.getElementById("Total").value = Total;
-} 	
+	// Calcul of Premium choice
+	if  (document.getElementById("PriceUnitPremium").checked)	{						 
+		document.getElementById("PriceUnit").value="12345.00";
+		install_Rate = (12345.00*(13/100)*qty_Elevators_quote).toFixed(2);
+		Total = (12345.00*1.13*qty_Elevators_quote).toFixed(2);
+		document.getElementById("InstallationCost").value = install_Rate ;
+		document.getElementById("Total").value = Total;
+	} 	
 
-if  (document.getElementById("PriceUnitExcelium").checked)	{						 
-document.getElementById("PriceUnit").value="15400.00";
-install_Rate = (15400.00*(16/100)*qty_Elevators_quote).toFixed(2);
-Total = (15400.00*1.16*qty_Elevators_quote).toFixed(2);//ok
-document.getElementById("InstallationCost").value = install_Rate ;
-document.getElementById("Total").value = Total;
-}								
+	// Calcul of Excelium choice
+	if  (document.getElementById("PriceUnitExcelium").checked)	{						 
+		document.getElementById("PriceUnit").value="15400.00";
+		install_Rate = (15400.00*(16/100)*qty_Elevators_quote).toFixed(2);
+		Total = (15400.00*1.16*qty_Elevators_quote).toFixed(2);
+		document.getElementById("InstallationCost").value = install_Rate ;
+		document.getElementById("Total").value = Total;
+	}								
 }
 
-function AddElementDiv1() {
-document.getElementById("DIV_1").style.display = "block";}
-function AddElementDiv2() {
-document.getElementById("DIV_2").style.display = "block";}
-function AddElementDiv3() {
-document.getElementById("DIV_3").style.display = "block";}
-function AddElementDiv4() {
-document.getElementById("DIV_4").style.display = "block";}
-function AddElementDiv5() {
-document.getElementById("DIV_5").style.display = "block";}
-function AddElementDiv6() {
-document.getElementById("DIV_6").style.display = "block";}
-function AddElementDiv7() {
-document.getElementById("DIV_7").style.display = "block";}
-function AddElementDiv8() {
-document.getElementById("DIV_8").style.display = "block";}
-function AddElementDiv9() {
-document.getElementById("DIV_9").style.display = "block";}						
-function AddElementDiv10() {
-document.getElementById("DIV_10").style.display = "block";}
-function AddElementDiv11() {
-document.getElementById("DIV_11").style.display = "block";}
-function AddElementDiv12() {
-document.getElementById("DIV_12").style.display = "block";}
-
-function RemoveElementDiv1() {
-document.getElementById("DIV_1").style.display = "none";}						
-function RemoveElementDiv2() {
-document.getElementById("DIV_2").style.display = "none";}						
-function RemoveElementDiv3() {
-document.getElementById("DIV_3").style.display = "none";}						
-function RemoveElementDiv4() {
-document.getElementById("DIV_4").style.display = "none";}						
-function RemoveElementDiv5() {
-document.getElementById("DIV_5").style.display = "none";}						
-function RemoveElementDiv6() {
-document.getElementById("DIV_6").style.display = "none";}						
-function RemoveElementDiv7() {
-document.getElementById("DIV_7").style.display = "none";}						
-function RemoveElementDiv8() {
-document.getElementById("DIV_8").style.display = "none";}						
-function RemoveElementDiv9() {
-document.getElementById("DIV_9").style.display = "none";}						
-function RemoveElementDiv10() {
-document.getElementById("DIV_10").style.display = "none";}
-function RemoveElementDiv11() {
-document.getElementById("DIV_11").style.display = "none";}
-function RemoveElementDiv12() {
-document.getElementById("DIV_12").style.display = "none";}
-
-//DEFAULT 
-function divStatus()	 {	
-		document.getElementById("PriceUnit").value = "";
-		document.getElementById("InstallationCost").value = "";
-		document.getElementById("Total").value = "";					
-	if (fonctionChoice_dropMenu()  === 0)  { 
-		 RemoveElementDiv1();
-		 RemoveElementDiv2();
-		 RemoveElementDiv3();
-		 RemoveElementDiv4();
-		 RemoveElementDiv5();
-		 RemoveElementDiv6();
-		 RemoveElementDiv7();
-		 RemoveElementDiv8();
-		 RemoveElementDiv9(); 
-		 RemoveElementDiv10();
-		 RemoveElementDiv11();
-		 RemoveElementDiv12();
-		 }  
-//RESIDENTIAL		
-	if	(fonctionChoice_dropMenu() === 1)	{	
-		AddElementDiv1();
-		AddElementDiv2();
-		AddElementDiv3();
-		AddElementDiv10()
-		RemoveElementDiv4();
-		RemoveElementDiv5();
-		RemoveElementDiv6();
-		RemoveElementDiv7();
-		RemoveElementDiv8();
-		RemoveElementDiv9();
-		RemoveElementDiv11();
-		RemoveElementDiv12();   
-		} 
-//COMMERCIAL											
-	if	(fonctionChoice_dropMenu() === 2)	{								
-		AddElementDiv2();
-		AddElementDiv3();
-		AddElementDiv4();
-		AddElementDiv5();
-		AddElementDiv6();
-		AddElementDiv11();
-		RemoveElementDiv1();
-		RemoveElementDiv7();
-		RemoveElementDiv8();
-		RemoveElementDiv9();
-		RemoveElementDiv10();
-		RemoveElementDiv12();
-		}  
-//CORPORATE									
-	if	(fonctionChoice_dropMenu() === 3)	{												
-		AddElementDiv2();
-		AddElementDiv3();
-		AddElementDiv5();
-		AddElementDiv7();
-		AddElementDiv8();
-		AddElementDiv12();
-		RemoveElementDiv1();
-		RemoveElementDiv4();
-		RemoveElementDiv6();
-		RemoveElementDiv9();
-		RemoveElementDiv10();
-		RemoveElementDiv11();
-		} 
-//HYBRID
-	if	(fonctionChoice_dropMenu() === 4)	{				
-		AddElementDiv2();
-		AddElementDiv3();
-		AddElementDiv4();
-		AddElementDiv5();
-		AddElementDiv8();
-		AddElementDiv9();
-		AddElementDiv12();
-		RemoveElementDiv1();
-		RemoveElementDiv6();
-		RemoveElementDiv7();
-		RemoveElementDiv10();
-		RemoveElementDiv11();
-		}
-
-		document.getElementById("qty_Pop").value = "";															  
-		document.getElementById("qty_Appart").value = ""; 
-		document.getElementById("qty_Base").value = ""; 
-		document.getElementById("qty_Business").value = "";
-		document.getElementById("qty_Eleva").value = "";								
-		document.getElementById("qty_Level").value = "";
-		document.getElementById("qty_loan").value = ""; 								
-		document.getElementById("qty_Parking").value = "";								
-		document.getElementById("qty_Hours").value = ""; 
-		document.getElementById("returned_qty_Eleva").value = "";
-		document.getElementById("hybrid").value = "";		
-		document.getElementById("returned_qty_ElevaB").value = "";												
-}
+	
